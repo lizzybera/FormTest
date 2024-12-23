984ae2471f180937fa0e8710cb1f3e431dd6500f
G.P SYSTEM SOLUTIONS <elizabethokorie407@gmail.com>
2024-12-23 20:42:28 +0100

    redirect

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/',  // Set this to '/' if you're deploying to the root domain or to '/your-project/' if it's a subpath.
-  // build: {
-  //   outDir: 'dist', // Ensure the output directory is set to 'dist' (default for Vite)
-  // },
 })
